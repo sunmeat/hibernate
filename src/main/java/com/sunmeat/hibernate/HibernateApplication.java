7c0e3f7254cc315299d7a85c9f538ab978cc05ab
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-11-07 15:36:26 +0200

    Update HibernateApplication.java

--- src/main/java/com/sunmeat/hibernate/HibernateApplication.java
+++ src/main/java/com/sunmeat/hibernate/HibernateApplication.java
@@ -3,11 +3,10 @@ package com.sunmeat.hibernate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// запуск приложения - http://localhost:8080/
-// должен быть поднят MySQL
+// запуск додатку - http://localhost:8080/
+// має бути піднятий MySQL
 @SpringBootApplication
 public class HibernateApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateApplication.class, args);
 	}
