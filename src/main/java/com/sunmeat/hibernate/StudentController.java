3f061a148e518cd5977ab5be505217c90daae037
Oleksandr Zahoruiko <sunmeatrich@gmail.com>
2025-11-07 15:41:15 +0200

    Update StudentController.java

--- src/main/java/com/sunmeat/hibernate/StudentController.java
+++ src/main/java/com/sunmeat/hibernate/StudentController.java
@@ -1,18 +1,15 @@
 package com.sunmeat.hibernate;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.OptimisticLockException;
+import jakarta.persistence.*;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
-
-	// кеширование Redis:
+	// кешування Redis:
 	// https://gist.github.com/sunmeat/62fa29183c71fe69ee9d35a9d2d9a218
 	
 	@Autowired
@@ -21,31 +18,31 @@ public class StudentController {
 	@Autowired
 	private StudentRepo studentRepository;
 
-	// получение всех студентов или одного по ID
+	// отримання всіх студентів або одного за ID
 	@GetMapping
 	public ResponseEntity<Object> getStudentOrAll(@RequestParam(name = "id", required = false) Long id) {
 		if (id != null) {
 			Student student = studentRepository.findById(id).orElseThrow(
-					() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Студент с ID " + id + " не найден"));
-			return ResponseEntity.ok(student); // возврат объекта студента в формате JSON
+					() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Студента з ID " + id + " не знайдено"));
+			return ResponseEntity.ok(student); // повернення об'єкта студента у форматі JSON
 		}
-		return ResponseEntity.ok(studentRepository.findAll()); // возврат всех студентов в формате JSON
+		return ResponseEntity.ok(studentRepository.findAll()); // повернення всіх студентів у форматі JSON
 	}
 
-	// удаление студента по ID
+	// видалення студента по ID
 	@PostMapping("/delete")
 	public ResponseEntity<String> deleteStudent(@RequestParam(name = "id") Long id) {
 		try {
 			studentService.deleteStudent(id);
-			return ResponseEntity.ok("Студент успешно удален.");
+			return ResponseEntity.ok("Студента успішно видалено.");
 		} catch (EntityNotFoundException e) {
-			return ResponseEntity.ok("Студент с ID " + id + " не найден.");
+			return ResponseEntity.ok("Студента з ID " + id + " не знайдено.");
 		} catch (OptimisticLockException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).body("Конфликт версии при удалении студента с ID " + id);
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Конфлікт версії при видаленні студента з ID" + id);
 		} catch (Exception e) {
-			System.out.println("Ошибка: " + e.getMessage());
+			System.out.println("Помилка: " + e.getMessage());
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body("Ошибка при удалении студента: " + e.getMessage());
+					.body("Помилка при видаленні студента: " + e.getMessage());
 		}
 	}
 
@@ -53,19 +50,19 @@ public class StudentController {
 	public ResponseEntity<String> saveStudent(@RequestParam(required = false, name = "_method") String _method,
 			@RequestParam(required = false, name = "id") Long id, @RequestParam(name = "name") String name,
 			@RequestParam(name = "email") String email) {
-		if ("PUT".equalsIgnoreCase(_method) && id != null) { // обновление существующего студента
+		if ("PUT".equalsIgnoreCase(_method) && id != null) { // оновлення існуючого студента
 			return studentRepository.findById(id).map(student -> {
 				student.setName(name);
 				student.setEmail(email);
 				studentRepository.save(student);
-				return ResponseEntity.ok("Студент обновлен: " + student.getName());
-			}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Студент не найден"));
-		} else { // создание нового студента
+				return ResponseEntity.ok("Студента оновлено: " + student.getName());
+			}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Студента не знайдено"));
+		} else { // створення нового студента
 			var newStudent = new Student();
 			newStudent.setName(name);
 			newStudent.setEmail(email);
 			studentRepository.save(newStudent);
-			return ResponseEntity.status(HttpStatus.CREATED).body("Студент создан: " + newStudent.getName());
+			return ResponseEntity.status(HttpStatus.CREATED).body("Студента створено: " + newStudent.getName());
 		}
 	}
 }
